c68a1090afc414b5a151cdaedb9f15328f88ffe4
emekdahl <emily@otus.com>
2020-07-21 08:45:23 -0500

    mode

--- statistics/central_tendencies.py
+++ statistics/central_tendencies.py
@@ -3,7 +3,7 @@ from typing import List
 
 import matplotlib.pyplot as plt
 
-num_friends = [100, 49, 41, 40, 25]
+num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
 
 num_friends_even = [100, 49, 41, 40, 25, 33]
 
@@ -12,14 +12,12 @@ num_friends_even = [100, 49, 41, 40, 25, 33]
 def mean(xs: List[float]) -> float:
     return sum(xs) / len(xs)
 
-assert mean(num_friends) == 51
+assert mean(num_friends) == 7.333333333333333
 
 
 def _median_odd(xs:List[float]) -> float:
     """ if len(xs) is odd, the median is the middle element"""
     return sorted(xs)[len(xs) // 2]
-
-assert _median_odd(num_friends) == 41
 
 
 def _median_even(xs:List[float]) -> float:
@@ -29,7 +27,6 @@ def _median_even(xs:List[float]) -> float:
 
     return (sorted(xs)[hi_midpoint - 1] + sorted_xs[hi_midpoint]) / 2
 
-assert _median_even(num_friends_even) == 40.5
 
 def median(v: List[float]) -> float:
     """Finds the middle-most value of v"""
@@ -40,3 +37,22 @@ assert median([1, 10, 2, 9, 5]) == 5
 assert median([1, 9, 2, 10]) == (2 + 9) / 2
 
 print(median(num_friends))
+
+def quantile(xs: List[float], p: float) -> float:
+    """ Returns the pth-percentile value in x"""
+    p_index = int(p*len(xs))
+    return sorted(xs)[p_index]
+
+assert quantile(num_friends, 0.10) == 1
+assert quantile(num_friends, 0.25) == 3
+assert quantile(num_friends, 0.75) == 9
+assert quantile(num_friends, 0.90) == 13
+
+def mode(x: List[float]) -> List[float]:
+    """Returns a list, since there might be more than one mode"""
+    counts = Counter(x)
+    max_count = max(counts.values())
+    return [x_i for x_i, count in counts.items()
+                if count == max_count]
+
+assert set(mode(num_friends)) == {1,6}
