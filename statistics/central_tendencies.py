cf58d6686d8998004872e845958432eba73cfa91
Em Ekdahl - Data Science <emekdahl-datascience@emilysmbploaner.lan>
2020-07-23 07:07:26 -0500

    standard deviation

--- statistics/central_tendencies.py
+++ statistics/central_tendencies.py
@@ -1,26 +1,36 @@
+import math
 from collections import Counter
 from typing import List
 
 import matplotlib.pyplot as plt
 
-num_friends = [100.0,49,41,40,25,21,21,19,19,18,18,16,15,15,15,15,14,14,13,13,13,13,12,12,11,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,9,8,8,8,8,8,8,8,8,8,8,8,8,8,7,7,7,7,7,7,7,7,7,7,7,7,7,7,7,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,6,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,5,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,4,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,3,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+num_friends = [100.0, 49, 41, 40, 25, 21, 21, 19, 19, 18, 18, 16, 15, 15, 15, 15, 14, 14, 13, 13, 13, 13, 12, 12, 11,
+               10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9,
+               9, 9, 9, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 7, 6, 6, 6, 6,
+               6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5,
+               4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3,
+               3, 3, 3, 3, 3, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+               1, 1, 1, 1, 1, 1, 1, 1, 1]
 
 num_friends_even = [100, 49, 41, 40, 25, 33]
 
+
 # Central Tendencies
+
 
 def mean(xs: List[float]) -> float:
     return sum(xs) / len(xs)
 
+
 assert mean(num_friends) == 7.333333333333333
 
 
-def _median_odd(xs:List[float]) -> float:
+def _median_odd(xs: List[float]) -> float:
     """ if len(xs) is odd, the median is the middle element"""
     return sorted(xs)[len(xs) // 2]
 
 
-def _median_even(xs:List[float]) -> float:
+def _median_even(xs: List[float]) -> float:
     """ if len(xs) is even, the median is the average of the two middle elements"""
     sorted_xs = sorted(xs)
     hi_midpoint = len(xs) // 2
@@ -38,41 +48,48 @@ assert median([1, 9, 2, 10]) == (2 + 9) / 2
 
 print(median(num_friends))
 
+
 def quantile(xs: List[float], p: float) -> float:
     """ Returns the pth-percentile value in x"""
-    p_index = int(p*len(xs))
+    p_index = int(p * len(xs))
     return sorted(xs)[p_index]
+
 
 assert quantile(num_friends, 0.10) == 1
 assert quantile(num_friends, 0.25) == 3
 assert quantile(num_friends, 0.75) == 9
 assert quantile(num_friends, 0.90) == 13
 
+
 def mode(x: List[float]) -> List[float]:
     """Returns a list, since there might be more than one mode"""
     counts = Counter(x)
     max_count = max(counts.values())
     return [x_i for x_i, count in counts.items()
-                if count == max_count]
+            if count == max_count]
 
-assert set(mode(num_friends)) == {1,6}
+
+assert set(mode(num_friends)) == {1, 6}
+
 
 # Dispersion
 
 def data_range(xs: List[float]) -> float:
     return max(xs) - min(xs)
 
-assert data_range(num_friends) == 99
 
+assert data_range(num_friends) == 99
 
 # Variance
 
 import linear_algebra.sum_of_squares as sum_of_squares
 
+
 def de_mean(xs: List[float]) -> List[float]:
     """Translate xs by subtracting its mean (so the result has mean 0)"""
     x_bar = mean(xs)
     return [x - x_bar for x in xs]
+
 
 def variance(xs: List[float]) -> float:
     """Almost the average squared deviation from the mean"""
@@ -82,4 +99,13 @@ def variance(xs: List[float]) -> float:
     deviations = de_mean(xs)
     return sum_of_squares(deviations) / (n - 1)
 
+
 assert 81.54 < variance(num_friends) < 81.55
+
+
+def standard_deviation(xs: List[float]) -> float:
+    """The standard deviation is the square root of the variance"""
+    return math.sqrt(variance(xs))
+
+
+assert 9.02 < standard_deviation(num_friends) < 9.04
